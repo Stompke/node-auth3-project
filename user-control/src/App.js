066d2bb28dd9ae8be41d8ac93af5b749c72bba14
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-26 15:45:50 -0800

    added front end app. not connected to api yet

---
+++ user-control/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
