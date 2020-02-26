277419d634f1a29a547036186e6b49c34c7e02f3
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-02-26 12:56:44 -0800

    server is runnin g

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
+
+
+module.exports = server;
+
+
+// function checkRole(role) {
+//   return (req, res, next) => {
+//     if(req.decodedToken && req.decodedToken.role && req.decodedToken.role.toLowerCase() === role) {
+//       next();
+//     } else {
+//       res.status(403).json({ message: "shall not pass!" });
+//     }
+//   }
+// }
